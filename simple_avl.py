46b17d3aa653062605c211b8ca4ca430776afd1d
Nauman <recluze@gmail.com>
2012-12-08 16:13:38 +0500

    Implemented case I and IV rotations

--- simple_avl.py
+++ simple_avl.py
@@ -9,55 +9,134 @@ class Node():
         self.parent = None 
         self.left = None 
         self.right = None 
-        self.height = 0 
-        
-    def __str__(self):
-        return str(self.key) + "(" + str(self.height) + ")"
-    
-    def is_leaf(self):
-        return (self.height == 0)
-    
 
 class AVLTree():
     def __init__(self, *args):
-        self.rootNode = None 
-        self.elements_cound = 0 
-        self.rebalance_count = 0 
+        self.node = None 
+        self.height = -1  
+        self.balance = 0; 
         
         if len(args) == 1: 
             for i in args[0]: 
                 self.insert(i)
                 
     def height(self):
-        if self.rootNode: 
-            return self.rootNode.height 
+        if self.node: 
+            return self.node.height 
         else: 
             return 0 
     
+    def is_leaf(self):
+        return (self.height == 0) 
+    
     def insert(self, key):
-        tree = self.rootNode
+        tree = self.node
         
         newnode = Node(key)
         
         if tree == None:
-            self.rootNode = newnode 
-            self.rootNode.left = AVLTree() 
-            self.rootNode.right = AVLTree()
+            self.node = newnode 
+            self.node.left = AVLTree() 
+            self.node.right = AVLTree()
             debug("Inserted key [" + str(key) + "]")
         
         elif key < tree.key: 
-            self.left.insert(self, key)
+            self.node.left.insert(key)
             
         elif key > tree.key: 
-            self.insert(self, key)
+            self.node.right.insert(key)
         
         else: 
             debug("Key [" + str(key) + "] already in tree.")
+            
+        # key inserted. Let's check if we're balanced
+        self.update_heights(False)
+        self.update_balances(False)
+        if self.balance > 1:
+            self.rrotate()
+            self.update_heights()
+            self.update_balances()
+        if self.balance < -1: 
+            self.lrotate() 
+            self.update_heights()
+            self.update_balances()
 
+
+            
+    def rrotate(self):
+        # Rotate left pivoting on self
+        debug ('Rotating ' + str(self.node.key) + ' right') 
+        A = self.node 
+        B = self.node.left.node 
+        T = B.right.node 
+        
+        self.node = B 
+        B.right.node = A 
+        A.left.node = T 
+
+    
+    def lrotate(self):
+        # Rotate left pivoting on self
+        debug ('Rotating ' + str(self.node.key) + ' left') 
+        A = self.node 
+        B = self.node.right.node 
+        T = B.left.node 
+        
+        self.node = B 
+        B.left.node = A 
+        A.right.node = T 
+        
+            
+    def update_heights(self, recurse=True):
+        if not self.node == None: 
+            if recurse: 
+                if self.node.left != None: 
+                    self.node.left.update_heights()
+                if self.node.right != None:
+                    self.node.right.update_heights()
+            
+            self.height = max(self.node.left.height,
+                              self.node.right.height) + 1 
+        else: 
+            self.height = -1 
+            
+    def update_balances(self, recurse=True):
+        if not self.node == None: 
+            if recurse: 
+                if self.node.left != None: 
+                    self.node.left.update_balances()
+                if self.node.right != None:
+                    self.node.right.update_balances()
+
+            self.balance = self.node.left.height - self.node.right.height 
+        else: 
+            self.balance = 0 
+            
+
+    def display(self, level=0, pref=''):
+        '''
+        Display the whole tree. Uses recursive def.
+        TODO: create a better display using breadth-first search
+        '''        
+        self.update_heights()  # Must update heights before balances 
+        self.update_balances()
+        if(self.node != None): 
+            print ' ' * level, pref, self.node.key, "[" + str(self.height) + ":" + str(self.balance) + "]", 'L' if self.is_leaf() else ' '    
+            if self.node.left != None: 
+                self.node.left.display(level + 1, '<')
+            if self.node.left != None:
+                self.node.right.display(level + 1, '>')
+        
 
 
 if __name__ == "__main__": 
     a = AVLTree() 
+    a.insert(5)
+    a.insert(4)
+    a.insert(3)
     a.insert(2)
-    a.insert(2)
+    a.insert(1)
+    a.insert(0)
+    a.display()
+
     
